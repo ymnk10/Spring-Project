cab5548d7c376ac3783696aabef585e9f905ad0b
ymnk10 <kseisaimin10@gmail.com>
2024-05-20 16:41:22 +0900

    ユーザ情報を検索するアプリのControllerクラスの実装

--- src/main/java/com/example/controller/Exam05Controller.java
+++ src/main/java/com/example/controller/Exam05Controller.java
@@ -1,2 +1,31 @@
-package com.example.controller;public class Exam05Controller {
+package com.example.controller;
+
+import com.example.domain.Member;
+import com.example.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/exam05")
+public class Exam05Controller {
+    @Autowired
+    private MemberRepository repository;
+
+    @GetMapping("")
+    public String index() {
+        return "exam05";
+    }
+
+    @PostMapping("/query-name")
+    public String queryName(String likeName, Model model) {
+        List<Member> memberList = repository.findLike(likeName);
+        model.addAttribute("memberList", memberList);
+        return "exam05-result";
+    }
 }
