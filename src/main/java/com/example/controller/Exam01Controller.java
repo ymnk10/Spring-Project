888ddd00bbcb7086092e09599f6d28d1191c9986
ymnk10 <kseisaimin10@gmail.com>
2024-05-20 13:59:18 +0900

    名前を表示するControllerクラスの実装

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -1,4 +1,23 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam")
 public class Exam01Controller {
+
+    @GetMapping("")
+    public String index(){
+        return "exam01";
+    }
+
+    @PostMapping("/show-name")
+    public String showName(String name, Model model){
+        model.addAttribute("name", name);
+        return "exam01-result";
+    }
 }
