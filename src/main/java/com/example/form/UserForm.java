b40f88b06776412024049a86c2592e487edb7808
ymnk10 <kseisaimin10@gmail.com>
2024-05-20 15:56:01 +0900

    プロフィールを表示するアプリのFormクラスの実装

--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,2 +1,47 @@
-package com.example.form;public class UserForm {
+package com.example.form;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserForm {
+    @NotBlank(message = "入力必須です。")
+    private String name;
+
+    @NotBlank(message = "入力必須です。")
+    private String age;
+
+    @NotBlank(message = "入力必須です。")
+    private String comment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
